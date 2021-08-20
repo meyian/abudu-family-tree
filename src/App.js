d088e1b3423384245bd01ad9272bd5272b7bb083
Hassan Abudu <meyian@gmail.com>
2021-08-20 09:31:15 +0000

    Add family tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+import FamilyChart from "./FamilyChart";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 style={{ textAlign: "center", fontFamily: "Helvetica" }}>
+          The Abudu Family Tree
+        </h1>
+      </div>
+      <FamilyChart />
     </div>
   );
 }
-
-export default App;
---
+++ src/FamilyChart.js
@@ -0,0 +1,43 @@
+import React from "react";
+import f3 from "family-chart";
+import data from "./data.json";
+
+export default class FamilyTree extends React.Component {
+  cont = React.createRef();
+
+  componentDidMount() {
+    if (!this.cont.current) return;
+    const store = f3.createStore({
+        data: data,
+        cont: this.cont.current,
+        card_display: [
+          (d) => `${d.data["first name"]} ${d.data["last name"]}` || "",
+          (d) => d.data.birthday || "",
+        ],
+        mini_tree: true,
+        hide_rels: true,
+        node_separation: 250,
+        level_separation: 150,
+        card_dim: {
+          w: 220,
+          h: 70,
+          text_x: 75,
+          text_y: 15,
+          img_w: 60,
+          img_h: 60,
+          img_x: 5,
+          img_y: 5,
+        },
+      }),
+      view = f3.d3AnimationView(store);
+
+    store.setOnUpdate((props) =>
+      view.update({ tree: store.state.tree, ...(props || {}) })
+    );
+    store.update.tree();
+  }
+
+  render() {
+    return <div className="family-chart" ref={this.cont}></div>;
+  }
+}
